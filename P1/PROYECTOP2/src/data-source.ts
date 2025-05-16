ac03febd54af3104f05dca312aa9ebbf2e5cc802
Walthergl66 <waltherjr68@gmail.com>
2025-05-15 21:29:07 -0500

    1

--- P1/PROYECTOP2/src/data-source.ts
+++ P1/PROYECTOP2/src/data-source.ts
@@ -1,4 +1,6 @@
-import {Cultivo} from "./models/cultivos";
+import {Cultivo, LecturaSensor, Sensor, User} from "./models/cultivos";
+import { Riego } from "./models/riego";
+import { ZonaCultivo } from "./models/zonaCultivo";
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 // import { View } from "./models/view";
@@ -11,8 +13,8 @@ export const AppDataSource = new DataSource({
     password: "Hola",   
     database: "cultivodb", 
     synchronize: true,
-    logging: false,
-    entities: [Cultivo],
+    logging: true,
+    entities: [Cultivo,User,Sensor, LecturaSensor,ZonaCultivo,Riego],
     subscribers: [],
     migrations: [],
 });
--- P1/PROYECTOP2/src/models/cultivos.ts
+++ P1/PROYECTOP2/src/models/cultivos.ts
@@ -1,4 +1,5 @@
-import {Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {Column, Entity, OneToMany, ManyToOne, JoinColumn, PrimaryGeneratedColumn } from "typeorm";
+
 
 
 @Entity()
@@ -20,9 +21,105 @@ export class Cultivo {
 
   @Column()
   fechaCosecha!: Date;
-
-
-
-//   @OneToMany(() => View, (view: View) => view.users)
-//   views!: View[];
 }
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column() 
+  username!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  role!: string; // 'admin' o 'user'
+
+}
+
+@Entity()
+export class Sensor {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  nombre!: string;
+
+  @Column()
+  tipo!: string;
+
+  @Column()
+  modelo!: string;
+
+  @Column()
+  unidadMedida!: string;
+
+  @Column()
+  estado! : 'activo' | 'inactivo' | 'mantenimiento';
+
+  @Column()
+  latitud!: number;
+
+  @Column()
+  longitud!: number;
+
+  @Column()
+  altura?: number;
+
+  @Column()
+  zonaCultivo!: 'norte' | 'sur' | 'este' | 'oeste' | 'centro';
+
+  @Column()
+  frecuenciaLectura!: number;
+
+  @Column()
+  ultimaLectura!: number;
+
+  @Column()
+  fechaUltimaLectura!: Date;
+
+  @Column()
+  rangoMin!: number;
+
+  @Column()
+  rangoMax!: number;
+
+  @OneToMany(() => LecturaSensor, lectura => lectura.sensor, {
+    cascade: true
+  })
+  historialLecturas!: LecturaSensor[];
+}
+
+@Entity()
+export class LecturaSensor {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  valor!: number;
+
+  @Column()
+  fechaHora!: Date;
+
+  @Column()
+  unidadMedida!: string;
+
+  @Column()
+  estadoAlerta!: 'normal' | 'critico' | 'fuera_rango';
+
+  @ManyToOne(() => Sensor, sensor => sensor.historialLecturas)
+  @JoinColumn({ name: 'sensorId' })
+  sensor!: Sensor;
+
+  @Column()
+  sensorId!: number;
+}
+
+
+
+
